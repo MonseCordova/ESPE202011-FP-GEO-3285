3c67d9656fce1f972c7b0785e102b8e6fd050e87
MonseCordova <juanamonsecordova@gmil.com>
2021-01-31 21:53:51 -0500

    HW08-FactorialNumber

---
+++ assignments/unit2/HW08-FactorialNumber/JavaApplication19/src/javaapplication19/JavaApplication19.java
@@ -0,0 +1,17 @@
+
+
+package javaapplication19;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class JavaApplication19 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
